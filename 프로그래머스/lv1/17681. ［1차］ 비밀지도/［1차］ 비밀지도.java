21eea6571d9036b568210ccd02ac6e26aac3fe52
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-19 16:51:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.21 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] arr5 = new String[n];
+        for (int i = 0; i < n; i++) {
+            String s = String.format("%"+n+"d", Long.parseLong(Integer.toBinaryString( arr1[i] | arr2[i])));
+            arr5[i] = s;
+            for (int j = 0; j < arr5[i].length(); j++) {
+                if (arr5[i].charAt(j) == '0' || arr5[i].charAt(j) == ' ') {
+                    arr5[i] = arr5[i].replaceAll("0", " ");
+                } else arr5[i] = arr5[i].replaceAll("1", "#");
+            }
+        }
+        return arr5;
+    }
+}
