0ec739b8a6bbd4696102b2d5a6fa4346f16c7143
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-22 20:46:40 +0900

    [level 1] Title: 모의고사, Time: 3.62 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,58 +1,20 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] answer) {
-        int[] result = {};
-        int[] supo_1st = {1,2,3,4,5};
-        int[] supo_2nd = {2,1,2,3,2,4,2,5};
-        int[] supo_3rd = {3,3,1,1,2,2,4,4,5,5};
-        
-        int cnt_1st = 0;
-        int cnt_2nd = 0;
-        int cnt_3rd = 0;
-        int max_score = -1;
-        int same_cnt = 0;
-        int max_idx = -1;
-        
-        for (int i = 0; i < answer.length; i++){
-            if(answer[i] == supo_1st[i % supo_1st.length]){
-                cnt_1st++;
-            }
-            if(answer[i] == supo_2nd[i % supo_2nd.length]){
-                cnt_2nd++;
-            }
-            if(answer[i] == supo_3rd[i % supo_3rd.length]){
-                cnt_3rd++;
-            }
-        }
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int[] score = new int[3];
-        score[0] = cnt_1st;
-        score[1] = cnt_2nd;
-        score[2] = cnt_3rd;
-        
-                for (int i = 0; i < score.length; i++) {
-            if(score[i] > max_score){
-                max_score = score[i];
-                max_idx = i;
-                same_cnt = 1;
-            }else if(score[i] == max_score){
-                same_cnt++;
-            }
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
-        if(same_cnt == 1){
-            result = new int[1];
-            result[0] = max_idx+1;
-        } else if(same_cnt > 1){
-            result = new int[same_cnt];
-            int x = 0;
-            for(int i = 0; i < score.length; i++) {
-                if (score[i] == max_score){
-                    result[x] = i+1;
-                    x = x + 1;
-                }
-            }
-        }
-        
-        
-        return result;
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
