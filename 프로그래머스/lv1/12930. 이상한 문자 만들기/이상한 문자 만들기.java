29d3f7a34569d318e1708cbe804a54a2e69fa0da
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-22 16:12:51 +0900

    Update 이상한 문자 만들기.java

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,31 +1,50 @@
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+// import java.util.ArrayList;
+// import java.util.StringTokenizer;
+
+// class Solution {
+//     public String solution(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         String result = "";
+//         s = s.toUpperCase();
+//         ////구분자 = " " , 구분자포함여부///
+//         StringTokenizer st = new StringTokenizer(s, " ", true);  
+                        
+//         ArrayList<String> list = new ArrayList<String>();
+        
+//         while (st.hasMoreTokens()) {
+//             list.add(st.nextToken());
+//         }
+//         for (int i = 0; i < list.size(); i++) {
+//             if (list.get(i).equals(" ")) {
+//                 sb.append(list.get(i));
+//             } else for (int j = 0; j < list.get(i).length(); j++) {
+//                 if (j % 2 == 0) {
+//                       sb.append((list.get(i).charAt(j)));
+//                 }else
+//                     sb.append(((char) (list.get(i).charAt(j) + 32)));
+//             }
+//         }
+//         result = sb.toString();
+      
+//         return result;
+//     }
+// }
+
+
 
 class Solution {
-    public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        String result = "";
-        s = s.toUpperCase();
-        ////구분자 = " " , 구분자포함여부///
-        StringTokenizer st = new StringTokenizer(s, " ", true);  
-                        
-        ArrayList<String> list = new ArrayList<String>();
-        
-        while (st.hasMoreTokens()) {
-            list.add(st.nextToken());
-        }
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(" ")) {
-                sb.append(list.get(i));
-            } else for (int j = 0; j < list.get(i).length(); j++) {
-                if (j % 2 == 0) {
-                      sb.append((list.get(i).charAt(j)));
-                }else
-                    sb.append(((char) (list.get(i).charAt(j) + 32)));
-            }
-        }
-        result = sb.toString();
-      
-        return result;
+    public String solution(String s, int n) {
+        char[] ss = s.toCharArray();
+
+		for (int i = 0; i < ss.length; i++) {
+			if (ss[i] == ' ') {
+				continue;
+			}
+
+            char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
+            ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
+		}
+
+		return new String(ss);
     }
 }
