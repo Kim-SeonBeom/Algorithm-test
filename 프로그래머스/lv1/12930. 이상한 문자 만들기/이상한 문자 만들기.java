c36ca8160431b7169e83c186abb97d2b8a274f14
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-27 16:23:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,31 +1,4 @@
-// import java.util.ArrayList;
-// import java.util.StringTokenizer;
 
-// class Solution {
-//     public String solution(String s) {
-//         StringBuilder sb = new StringBuilder();
-//         String result = "";
-//         s = s.toUpperCase();
-//         ////구분자 = " " , 구분자포함여부///
-//         StringTokenizer st = new StringTokenizer(s, " ", true);  
-//         ArrayList<String> list = new ArrayList<String>();
-//         while (st.hasMoreTokens()) {
-//             list.add(st.nextToken());
-//         }
-//         for (int i = 0; i < list.size(); i++) {
-//             if (list.get(i).equals(" ")) {
-//                 sb.append(list.get(i));
-//             } else for (int j = 0; j < list.get(i).length(); j++) {
-//                 if (j % 2 == 0) {
-//                       sb.append((list.get(i).charAt(j)));
-//                 }else
-//                     sb.append(((char) (list.get(i).charAt(j) + 32)));
-//             }
-//         }
-//         result = sb.toString();
-//         return result;
-//     }
-// }
 
 class Solution {
     public String solution(String s) {
@@ -37,8 +10,10 @@ class Solution {
             
             if(wordIdx %2 != 0 ){
                 s_split[i] = (char)(s_split[i]-32);
+            
             }
         }
+      
         return new String(s_split);
     }
 }
