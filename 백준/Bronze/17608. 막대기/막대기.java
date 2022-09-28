6e1624fe3498aa8737399a2febdbce5d3727323e
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-28 13:29:02 +0900

    [Bronze II] Title: 막대기, Time: 924 ms, Memory: 112668 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -2,31 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-      Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) {
+
       Scanner sc = new Scanner(System.in);
-      ArrayList<Integer> list = new ArrayList<>();
       int n = sc.nextInt();
-      for (int i = 0; i < n; i++) {
-          stack.push(sc.nextInt());
+      int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+          arr[i] = sc.nextInt();
       }
-      int[] arr = new int[stack.size()];
-      for (int i = 0; i < arr.length; i++){
-          arr[i] = stack.pop();
-      }
-      int max = arr[0];
+      int max = arr[arr.length - 1];
       int cnt = 1;
-      for (int i = 0; i < arr.length; i++){
+      for (int i = arr.length-1; i >= 0; i--){
           if(max<arr[i]){
               max = arr[i];
               cnt++;
           }
       }
         System.out.println(cnt);
-
-
-
-
     }
 
 }
