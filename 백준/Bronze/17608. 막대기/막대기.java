fafba9a94d18a5a75279ebf2c3fc503002271fbf
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-28 13:35:48 +0900

    [Bronze II] Title: 막대기, Time: 252 ms, Memory: 22492 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -2,24 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
-      int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-          arr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+          a[i] = Integer.parseInt(br.readLine());
       }
-      int max = arr[arr.length - 1];
+      int max = a[a.length - 1];
       int cnt = 1;
-      for (int i = arr.length-1; i >= 0; i--){
-          if(max<arr[i]){
-              max = arr[i];
+      for (int i = a.length-1; i >= 0; i--){
+          if(max<a[i]){
+              max = a[i];
               cnt++;
           }
       }
-        System.out.println(cnt);
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      bw.write(String.valueOf(cnt));
+      bw.close();
+
     }
 
 }
