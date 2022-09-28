538eb0a90c7d8bc2f3d34e8c6545c2d33c1ebf9f
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-29 00:14:16 +0900

    [level 2] Title: 올바른 괄호, Time: 7.15 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,49 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     boolean solution(String s) {
+//         Queue<Character> queue = new LinkedList<>();
+//         for(int i = 0; i< s.length(); i++){
+//             queue.add(s.charAt(i));
+//         }
+//         int cnt = 0;
+        
+//             while(queue.size()>0){
+//                 if(queue.poll().equals('(')) {
+//                     cnt++;
+//                 }else cnt--;
+                
+//                 if(cnt<0){
+//                     return false;
+//                 }
+//             }
+//             if(cnt == 0){
+//                 return true;
+//             }else return false;
+//         }
+//     }
 class Solution {
     boolean solution(String s) {
-        Queue<Character> queue = new LinkedList<>();
-        for(int i = 0; i< s.length(); i++){
-            queue.add(s.charAt(i));
-        }
-        int cnt = 0;
-        
-            while(queue.size()>0){
-                if(queue.poll().equals('(')) {
-                    cnt++;
-                }else cnt--;
-                
-                if(cnt<0){
-                    return false;
+       boolean answer = true;
+        char[] carray = s.toCharArray();
+        int cnt=0;
+        for(char c : carray){
+            if(c=='('){
+                cnt++;
+            }else if(c==')'){
+                if(cnt>0){
+                    cnt--;
+                }else {
+                    answer=false;
+                    break;
                 }
             }
-            if(cnt == 0){
-                return true;
-            }else return false;
         }
+        if(cnt!=0){
+            answer=false;
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
+}
