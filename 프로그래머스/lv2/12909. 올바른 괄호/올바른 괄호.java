251aaefc8866fcad1b33b3adb2fc8561841777eb
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-29 00:15:28 +0900

    [level 2] Title: 올바른 괄호, Time: 8.46 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -23,26 +23,22 @@
 //     }
 class Solution {
     boolean solution(String s) {
-       boolean answer = true;
-        char[] carray = s.toCharArray();
-        int cnt=0;
-        for(char c : carray){
-            if(c=='('){
-                cnt++;
-            }else if(c==')'){
-                if(cnt>0){
-                    cnt--;
-                }else {
-                    answer=false;
-                    break;
-                }
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
             }
         }
-        if(cnt!=0){
-            answer=false;
+        if(count == 0){
+            answer = true;
         }
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
 
         return answer;
     }
