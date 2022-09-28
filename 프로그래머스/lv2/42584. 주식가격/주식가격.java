d2bc7733d403f8258767012d3f0524e5d67480fc
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-09-28 14:31:48 +0900

    [level 2] Title: 주식가격, Time: 10.33 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            int cnt = 0;
+            for (int j = i+1; j < prices.length; j++) {
+                if (prices[i] <= prices[j]) {
+                    cnt++;
+                } else{
+                    cnt++;
+                    break;
+                }
+            }
+            result[i] = cnt;
+        }
+        
+        return result;
+    }
+}
