c2ead08ee5ab0c38982dc735e11f66cab326ee6b
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-04 16:11:44 +0900

    [level 2] Title: 전화번호 목록, Time: 651.33 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,7 +4,7 @@ class Solution {
     public boolean solution(String[] numbers) {
         boolean result = true;
         Arrays.sort(numbers);
-     
+
         for (int i = 0; i < numbers.length-1; i++) {
             if (numbers[i + 1].startsWith(numbers[i])) {
                 result = false;
