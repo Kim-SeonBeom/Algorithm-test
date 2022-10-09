d7570dbcb24262db379439d204582e9b31a6c7bc
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-09 16:33:29 +0900

    [level 2] Title: 더 맵게, Time: 1887.90 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int cnt = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+        while(pq.peek()<=K && pq.size()>1){
+           pq.add(pq.poll() + 2*pq.poll());
+            cnt++;
+        }
+        if(pq.size()<1||pq.peek()<=K){
+            return -1;
+        }else return cnt;
+      
+    }
+}
