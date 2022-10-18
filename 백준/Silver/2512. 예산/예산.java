0cf50d5820b367303fbd480cd60dec317f12e45e
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-18 22:38:10 +0900

    [Silver III] Title: 예산, Time: 172 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] n_arr;
+    static int m;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        n = Integer.parseInt(s);
+        s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        n_arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            n_arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(n_arr);
+        m = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int left = 0;
+        int right = n_arr[n_arr.length - 1];
+        int mid = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            sum = 0;
+      //      System.out.println("mid = " + mid);
+     //       System.out.println("right = " + right);
+     //       System.out.println("left = " + left);
+            for (int i = 0; i < n_arr.length; i++) {
+                if (n_arr[i] < mid) {
+                    sum += n_arr[i];
+                } else sum += mid;
+            }
+     //       System.out.println("sum = " + sum);
+            if (sum > m) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+   //         System.out.println();
+        }
+    //    System.out.println("mid = " + mid);
+    //    System.out.println("right = " + right);
+    //    System.out.println("left = " + left);
+        System.out.println(right);
+
+
+    }
+}
