61b74be0bde3fb999979676b0cdc13e9818243c2
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-22 03:24:08 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 7 || n == 4) {
+            System.out.println(-1);
+        } else if (n % 5 == 0) {
+            System.out.println(n / 5);
+        } else if ((n % 5) % 2 != 0) {
+            System.out.println((n / 5) + 1);
+        } else if ((n % 5) % 2 == 0) {
+            System.out.println((n / 5) + 2);
+        }
+    }
+}
