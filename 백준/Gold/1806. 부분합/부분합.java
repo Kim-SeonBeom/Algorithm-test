f8f0c97f04138275592da2e84459076559eed63b
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-24 04:12:00 +0900

    [Gold IV] Title: 부분합, Time: 300 ms, Memory: 23960 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        int length = Integer.MAX_VALUE;
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        while (left < n) {
+            if (sum > m || right == n) {
+                sum -= a[left++];
+            } else sum += a[right++];
+
+            if (sum >= m) {
+                length = Math.min(length, right - left);
+            }
+        }
+        if (length == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(length);
+        }
+    }
+}
+
