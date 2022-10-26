6dfba9f1597a0299d44c9ea938f2adc5d90de732
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2022-10-27 07:48:01 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Main {
@@ -13,31 +10,48 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
         StringTokenizer st = new StringTokenizer(s);
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>(Comparator.reverseOrder());
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+//        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Integer [] arr1 = new Integer[n];
+        int [] arr2 = new int[n];
+//        int [] arr3 = new int[n];
 
+//        for (int i = 0; i < n; i++) {
+//            pq.add(Integer.parseInt(st.nextToken()));
+//        }
         for (int i = 0; i < n; i++) {
-            pq1.add(Integer.parseInt(st.nextToken()));
+           arr1[i] = Integer.parseInt(st.nextToken());
+//           arr3[i] = arr1[i];
         }
         s = br.readLine();
         st = new StringTokenizer(s);
         for (int i = 0; i < n; i++) {
-            pq2.add(Integer.parseInt(st.nextToken()));
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
+//        System.out.println(Arrays.toString(arr1));
+//        System.out.println(Arrays.toString(arr2));
+//        System.out.println(Arrays.toString(arr3));
+
+//        for (int i = 0; i < n; i++){
+//            int idx = -1;
+//            int max = -1;
+//            for (int j = 0; j < n; j++) {
+//                if(arr3[j]>max){
+//                    max = arr3[j];
+//                    idx = j;
+//                }
+//            }
+//            arr2[idx] = pq.poll();
+//            arr3[idx] = 0;
+//        }
+        Arrays.sort(arr1,Comparator.reverseOrder());
+        Arrays.sort(arr2);
+
+//        System.out.println(Arrays.toString(arr3));
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += pq1.poll() * pq2.poll();
+            sum += arr1[i] * arr2[i];
         }
         System.out.println(sum);
-
-
-//        System.out.println(Arrays.toString(arr));
-//        int left = 0;
-//        int right = 0;
-//        int max = Integer.MIN_VALUE;
-//        int sum = 0;
-//        int result_l = 0;
-
 
     }
 
