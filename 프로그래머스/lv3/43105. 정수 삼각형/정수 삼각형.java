cdf7c2ee2e3dd297e632cf3ac2a35b9d6da731a9
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2023-03-27 04:39:42 +0900

    [level 3] Title: 정수 삼각형, Time: 8.06 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,27 +1,26 @@
-import java.util.Arrays;
+
 class Solution {
     public int solution(int[][] triangle) {
-        int[][] dp = Arrays.copyOf(triangle, triangle.length);
+        int answer = 0;
         for (int i = 0; i < triangle.length; i++) {
             for (int j = 0; j < triangle[i].length; j++) {
                 if (i == 0) {
-                    dp[i][j] = triangle[i][j];
+                    triangle[i][j] = triangle[i][j];
                 } else {
                     if (j == 0) {
-                        dp[i][j] = dp[i - 1][0] + triangle[i][j];
+                        triangle[i][j] = triangle[i - 1][0] + triangle[i][j];
                     } else if (j == triangle[i].length - 1) {
-                        dp[i][j] = dp[i - 1][dp[i - 1].length - 1] + triangle[i][j];
+                        triangle[i][j] = triangle[i - 1][triangle[i - 1].length - 1] + triangle[i][j];
                     } else {
-                        dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                        triangle[i][j] = Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]) + triangle[i][j];
                     }
                 }
             }
         }
-        int max = 0;
-        for (int i = 0; i < dp.length; i++) {
-            max = Math.max(dp[dp.length - 1][i], max);
+        for (int i = 0; i < triangle.length; i++) {
+            answer = Math.max(triangle[triangle.length - 1][i], answer);
         }
         
-        return max;
+        return answer;
     }
 }
