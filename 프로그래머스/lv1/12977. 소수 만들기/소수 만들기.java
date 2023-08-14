a2efcabbf478f858ae43f5c67e1772c6d35a6584
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2023-08-14 18:06:29 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,39 @@
+class Solution {
+        public  boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public  int countPrimeSums(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    public int solution(int[] arr) {
+        
+int result = countPrimeSums(arr);
+        return result;
+    }
+}
