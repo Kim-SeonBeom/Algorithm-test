3f4e4c4b7c4f8d96211bbc3b3db034f5263d5694
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2023-08-15 15:24:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.05 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,28 +1,17 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
+    
         String answer = "";
-                HashMap<String, Integer> notComplete = new HashMap<>();
-        for (String x : participant) {
-            if (notComplete.containsKey(x)) {
-                notComplete.put(x, notComplete.get(x) + 1);
-            }else            notComplete.put(x, 1);
-        }
-        for (String x : completion) {
-            if (notComplete.containsKey(x) && notComplete.get(x) > 1) {
-                notComplete.put(x, notComplete.get(x) - 1);
-            } else if (notComplete.containsKey(x) && notComplete.get(x) == 1) {
-                notComplete.remove(x);
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
         }
-        String s = "";
-        for (String key : notComplete.keySet()) {
-            // 키와 매핑된 값이랑 equals() 메서드에 전달된 값이랑 일치하면 반복문을 종료합니다.
-            if (notComplete.get(key).equals(1)) { // 키가 null이면 NullPointerException 예외 발생
-                s = key;
-                break;
-            }
-        }
-        return s;
+        return answer;
     }
 }
