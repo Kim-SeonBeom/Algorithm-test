56e08e50455d17f41d404aef1bbeeb6c94adc2e3
Kim-SeonBeom <113013495+Kim-SeonBeom@users.noreply.github.com>
2023-09-14 14:23:20 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+     
+        int times = 1;
+        int[] chk = new int[10];
+        int max = 9;
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[]{i, priorities[i]});
+            chk[priorities[i]]++;
+        }
+
+        while (true) {
+            if (chk[max] == 0) {
+                max--;
+                continue;
+            }
+            if(queue.peek()[1]!=max){
+                queue.add(queue.poll());
+            } else {
+                if(queue.poll()[0]!=location){
+                    times++;
+                    chk[max]--;
+                }else break;
+            }
+        }
+        return times;
+    }
+}
